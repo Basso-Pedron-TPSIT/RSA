6d69cca8e4b15917aeef7fde494d923735df5518
MatteoBax <98881971+MatteoBax@users.noreply.github.com>
2024-11-14 16:54:08 +0100

    Created RSA class and added loggers

--- src/main/java/com/itts/volterra/quinta/BassoPedron/RSA/App.java
+++ src/main/java/com/itts/volterra/quinta/BassoPedron/RSA/App.java
@@ -1,6 +1,10 @@
 package com.itts.volterra.quinta.BassoPedron.RSA;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
+	private static final Logger logger = LogManager.getLogger(App.class);
 	public static void main(String[] args) {
 		// TODO
 	}
---
+++ src/main/java/com/itts/volterra/quinta/BassoPedron/RSA/RSA.java
@@ -0,0 +1,8 @@
+package com.itts.volterra.quinta.BassoPedron.RSA;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class RSA {
+	private static final Logger logger = LogManager.getLogger(RSA.class);
+}
