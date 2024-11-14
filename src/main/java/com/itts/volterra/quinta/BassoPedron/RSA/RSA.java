e19a412a501319de407edaa442cbd7c24e8aaec7
MatteoBax <98881971+MatteoBax@users.noreply.github.com>
2024-11-14 20:28:08 +0100

    Moved the contents of the main method into the RSA constructor

--- src/main/java/com/itts/volterra/quinta/BassoPedron/RSA/RSA.java
+++ src/main/java/com/itts/volterra/quinta/BassoPedron/RSA/RSA.java
@@ -6,65 +6,69 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 public class RSA {
-	  private static final Logger logger = LogManager.getLogger(RSA.class);
+	private static final Logger logger = LogManager.getLogger(RSA.class);
+	
+	public RSA(int bitLength) {
+		SecureRandom random = new SecureRandom();
+		// Genera due numeri primi casuali, entrambi maggiori di almeno 300 bit
+        BigInteger p = generatePrime(random, 300);
+        BigInteger q = generatePrime(random, 300);
 
-	    // Funzione per generare un numero primo maggiore di almeno minLength bit 
-	    public static BigInteger generatePrime(SecureRandom random, int minLength) {
-	        BigInteger prime;
-	        
-	        do {
-	            prime = new BigInteger(minLength, random);
-	        } while (!prime.isProbablePrime(100)); // finché non troviamo un numero primo
+        // Calcolo n 
+        BigInteger n = p.multiply(q);
+        
+        BigInteger P = p.subtract(BigInteger.ONE);
+        BigInteger Q = q.subtract(BigInteger.ONE);
+        // Calcolo z
+        BigInteger z = P.multiply(Q);
+        
+        // Calcolo la chiave pubblica
+        BigInteger e = generateCoprime(z);
+        
+        // mostra i risultati nel log
+        logger.info("Numeri primi generati:");
+        logger.info("p: " + p);
+        logger.info("q: " + q);
+        logger.info("n = p * q: " + n);
+        logger.info("z = (p - 1) * (q - 1): " + z);
+        logger.info("ESPONENTE PUBBLICO");
+        logger.info("Numero coprimo generato (e): " + e);
+		
+	}
+    // Funzione per generare un numero primo maggiore di almeno bitLength bit 
+    public BigInteger generatePrime(SecureRandom random, int bitLength) {
+        BigInteger prime;
+        
+        do {
+            prime = new BigInteger(bitLength, random);
+        } while (!prime.isProbablePrime(100)); // finché non troviamo un numero primo
 
-	        return prime;
-	    }
+        return prime;
+    }
 	    
 	    
-	    // Funzione per calcolare il massimo comun divisore (gcd)
-	    public static BigInteger gcd(BigInteger p, BigInteger q) {
-	        return p.gcd(q); // metodo della classe BigInteger
-	    }
+    // Funzione per calcolare il massimo comun divisore (gcd)
+    public BigInteger gcd(BigInteger p, BigInteger q) {
+        return p.gcd(q); // metodo della classe BigInteger
+    }
 	    
-	    // Funzione per generare un numero coprimo e con z
-	    public static BigInteger generateCoprime(BigInteger z) {
-	        SecureRandom random = new SecureRandom();
-	        BigInteger e;
+    // Funzione per generare un numero coprimo e con z
+    public BigInteger generateCoprime(BigInteger z) {
+        SecureRandom random = new SecureRandom();
+        BigInteger e;
 
-	        do {
-	            e = new BigInteger(z.bitLength(), random);
-	        } while (e.compareTo(BigInteger.ONE) <= 0 || e.compareTo(z) >= 0 || gcd(e, z).compareTo(BigInteger.ONE) != 0);
+        do {
+            e = new BigInteger(z.bitLength(), random);
+        } while (e.compareTo(BigInteger.ONE) <= 0 || e.compareTo(z) >= 0 || gcd(e, z).compareTo(BigInteger.ONE) != 0);
 
-	        return e; // Restituisce il numero coprimo
-	    }
+        return e; // Restituisce il numero coprimo
+    }
 
 	    public static void main(String[] args) {
 	        try {
 	           
-	            SecureRandom random = new SecureRandom();
+	            
 
-	            // Genera due numeri primi casuali, entrambi maggiori di almeno 300 bit
-	            BigInteger p = generatePrime(random, 300);
-	            BigInteger q = generatePrime(random, 300);
-
-	            // Calcolo n 
-	            BigInteger n = p.multiply(q);
-	            
-	            BigInteger P = p.subtract(BigInteger.ONE);
-	            BigInteger Q = q.subtract(BigInteger.ONE);
-	            // Calcolo z
-	            BigInteger z = P.multiply(Q);
-	            
-	            // Calcolo la chiave pubblica
-	            BigInteger e = generateCoprime(z);
-	            
-	            // mostra i risultati nel log
-	            logger.info("Numeri primi generati:");
-	            logger.info("p: " + p);
-	            logger.info("q: " + q);
-	            logger.info("n = p * q: " + n);
-	            logger.info("z = (p - 1) * (q - 1): " + z);
-	            logger.info("ESPONENTE PUBBLICO");
-	            logger.info("Numero coprimo generato (e): " + e);
 	        } catch (Exception e) {
 	            logger.error("Errore durante la generazione dei numeri primi!", e);
 	        }
