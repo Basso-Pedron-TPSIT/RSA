b79cf9b765dee590af7729b30c7a7dff5e55ffe3
MatteoBax <98881971+MatteoBax@users.noreply.github.com>
2024-11-15 18:08:22 +0100

    Writed JavaDoc for Tools class

--- src/main/java/com/itts/volterra/quinta/BassoPedron/RSA/Tools.java
+++ src/main/java/com/itts/volterra/quinta/BassoPedron/RSA/Tools.java
@@ -2,7 +2,20 @@ package com.itts.volterra.quinta.BassoPedron.RSA;
 
 import java.util.ArrayList;
 
+/**
+ * Classe Tools
+ * Contiene metodi utili
+ * 
+ * @author Matteo Basso, Lorenzo Pedron
+ */
 public class Tools {
+	/**
+	 * Divide la stringa in sottostringhe della lunghezza specificata
+	 * 
+	 * @param string la stringa da dividere
+	 * @param numberOfChars il massimo numero di caratteri che deve avere avere la sottostringa
+	 * @return l'ArrayList contenente le sottostringhe
+	 */
 	public static ArrayList<String> splitStringInSubStringOfNchars(String string, int numberOfChars) {
 		ArrayList<String> splitted = new ArrayList<String>();
 		int startIndex = 0;
